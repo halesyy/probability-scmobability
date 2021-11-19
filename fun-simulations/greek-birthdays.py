f435c8e0c6fc4d21462d5f6388a5dad209a68ded
Jack Hales <halesyy@gmail.com>
2021-11-19 15:02:22 +1100

    working sim

--- fun-simulations/greek-birthdays.py
+++ fun-simulations/greek-birthdays.py
@@ -11,5 +11,40 @@ conventionally, this number is expected
 to be: 365/2
 """
 
-def history(desire=50):
-    pass
+from datetime import datetime
+from random import randint
+
+random_date = lambda: str(datetime.fromtimestamp(randint(1609419600,1640869200))).split(" ")[0]
+
+# history, for given people
+def history(people=2) -> int:
+    def count(birthdays):
+        counted = {}
+        for bd in birthdays:
+            if bd not in counted:
+                counted[bd] = 0
+            counted[bd] += 1
+        return counted
+    # desired_probability = desire / 100 # 0.5 if 50
+    people = people # the # of people
+    birthdates = [random_date() for _ in range(people)]
+    values = [x for x in count(birthdates).values() if x > 1]
+    return len(values) > 0
+
+def correct_for(people, hists):
+    histories = [history(people) for _ in range(hists)]
+    return sum(histories)/hists
+
+# compile results for # of people in group, alternate histories
+results = []
+for i in range(50): # the people
+    if i == len(results):
+        results.append([])
+    for _ in range(75): # samples
+        results[i].append(correct_for(i, 1000))
+
+# compile tsv report
+report = ""
+for i, res in enumerate(results):
+    report += str(i) + "\t" + "{:.2f}%".format(sum(res)/len(res)*100) + "\n" # avg
+open("results/birthdays.tsv", "w").write(report)
