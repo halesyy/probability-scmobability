4ab6672f7d18885ed2108cc21cbf5bb46e3a43eb
Jack Hales <halesyy@gmail.com>
2021-11-19 13:43:14 +1100

    asd

---
+++ fun-simulations/greek-birthdays.py
@@ -0,0 +1,15 @@
+
+"""
+PROBLEM: how many people do you need in a
+group in order for the probability of two
+individuals to have the same birthday be
+X%?
+e.g., how many people need to be in a group
+for there to be a 50% chance of two people
+having the same birthdate...
+conventionally, this number is expected
+to be: 365/2
+"""
+
+def history(desire=50):
+    pass
