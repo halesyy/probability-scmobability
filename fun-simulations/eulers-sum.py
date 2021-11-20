2836c96aaa07d708a19068a61af3e9d086aa5c22
Jack Hales <halesyy@gmail.com>
2021-11-20 14:23:29 +1100

    eulers is dope

--- fun-simulations/eulers-sum.py
+++ fun-simulations/eulers-sum.py
@@ -12,8 +12,18 @@ def sum_to():
         iters += 1
     return iters
 
+# a iter-then-calculate approach.
 def history(iters=1000):
     results = [sum_to() for _ in range(iters)]
     return sum(results)/len(results)
 
-print("eulers number is approximately...", history(10000000))
+# a calculate-as-iter approach.
+def sequential_history(iters=3):
+    value = 0
+    for i in range(iters):
+        value += sum_to()
+    average = value/(i+1)
+    return average
+
+# sequential_history()
+print("eulers number is approximately...", sequential_history(1000000))
